0b947b8093d7f6300064ee1fbc0899c81781bd32
NOC_EO <cneil@noc.ac.uk>
2023-12-02 00:26:02 +0000

    HOTB

--- rf4eo_classify.py
+++ rf4eo_classify.py
@@ -4,8 +4,8 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, \
-    confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, classification_report
+#    confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 
 from src.utils.naming import images_dir_path, classified_file_paths
@@ -49,36 +49,34 @@ class RF4EO_Classify(object):
                                              attribute=CLASSIFICATION_ATTR)
             labels = np.unique(training_np[training_np > 0])
 
+            # split the training data 80:20 into training and validation data
             X = image_np[training_np > 0, :]
             y = training_np[training_np > 0]
-            X_train, X_test, y_train, y_test = train_test_split(X, y,
+            X_train, X_validation, y_train, y_validation = train_test_split(X, y,
                                                                 train_size=0.8,
                                                                 test_size=0.2,
                                                                 random_state=42)
 
+            # create a Random Forest classifier
             classifier = RandomForestClassifier(n_estimators=NUMBER_OF_TREES,
                                                 bootstrap=True,
                                                 oob_score=True,
                                                 verbose=VERBOSE,
                                                 n_jobs=NUMBER_OF_CORES)
 
+            # train the classifier
             X_train = np.nan_to_num(X_train)
             fit_estimator = classifier.fit(X_train, y_train)
 
-            # With our Random Forest model fit, we can check out the "Out-of-Bag" (OOB) prediction score:
-            processing_logger.info(msg='-------------------------------------------------')
-            processing_logger.info(msg='TRAINING and RF Model Diagnostics:')
+            # log the relative importance of each band
             processing_logger.info(msg='')
-
-            # we can show the band importance:
+            processing_logger.info(msg='importance of each band')
             bands = range(1, number_bands + 1)
             for band, importance in zip(bands, fit_estimator.feature_importances_):
                 processing_logger.info(msg=f'band {band} importance: {importance:.2f}')
             processing_logger.info(msg='')
 
-            # Now predict for each pixel
-            # first prediction will be tried on the entire image
-            # if not enough RAM, the dataset will be sliced
+            # classify the entire image
             new_shape = (image_np.shape[0] * image_np.shape[1], image_np.shape[2])
             image_ar = image_np.reshape(new_shape)
             image_ar = np.nan_to_num(image_ar)
@@ -88,25 +86,27 @@ class RF4EO_Classify(object):
             except MemoryError:
                 print_debug(msg=f'MemoryError: need to slice ...')
 
+            # save classified image to file
             write_geotiff(output_array=classified_image_np.reshape((y_size, x_size)),
                           output_file_path=classified_file_paths(self.Config, image_name),
                           geometry=geometry)
 
-            X_test = np.nan_to_num(X_test)
-            y_predict = classifier.predict(X_test)
-
-            # Confusion matrix
-            convolution_mat = pd.crosstab(y_test, y_predict, margins=True)
-            processing_logger.info(msg=convolution_mat)
+            # perform accuracy analysis and log the results
+            # first the confusion matrix
+            X_validation = np.nan_to_num(X_validation)
+            y_predict = classifier.predict(X_validation)
+            confusion_matrix = pd.crosstab(y_validation, y_predict, margins=True)
+            processing_logger.info(msg=confusion_matrix)
             processing_logger.info(msg='')
 
+            # and then the classification report
             target_names = [str(name) for name in range(1, len(labels) + 1)]
-            class_report = classification_report(y_test, y_predict, target_names=target_names)
+            class_report = classification_report(y_validation, y_predict, target_names=target_names)
             processing_logger.info(msg=class_report)
             processing_logger.info(msg='')
 
-            # Overall Accuracy (OAA)
-            message = f'OAA = {(accuracy_score(y_test, y_predict) * 100):.1f}%'
+            # lastly log the overall accuracy and send to console
+            message = f'OAA = {(accuracy_score(y_validation, y_predict) * 100):.1f}%'
             print_debug(msg=message)
             processing_logger.info(msg=message)
 
